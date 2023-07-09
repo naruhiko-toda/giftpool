77f341c365eb14283ddee63b0ec4825a383f0c5c
naruhiko-toda <nr27bypphy@gmail.com>
2023-07-10 00:42:37 +0900

    rename

--- test/index.test.tsx
+++ test/index.test.tsx
@@ -1,6 +1,7 @@
 import { render, screen } from '@testing-library/react'
 import React from "react";
-import Page from "@/app/page";
+import Page from "../app/page";
+
 
 describe('Home', () => {
   it('renders a heading', () => {
