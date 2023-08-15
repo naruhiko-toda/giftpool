c8eefbbb2f3008da35b80494fbae9040ebea5192
naruhiko-toda <nr27bypphy@gmail.com>
2023-08-15 21:12:09 +0900

    add eslint

--- e2e/home.spec.ts
+++ e2e/home.spec.ts
@@ -1,8 +1,8 @@
-import { test, expect } from '@playwright/test';
+import { test, expect } from "@playwright/test";
 
-test('レッスン予約のストーリー', async ({ page }) => {
-  await page.goto('localhost:8080/');
+test("レッスン予約のストーリー", async ({ page }) => {
+  await page.goto("localhost:8080/");
 
   // Expect a title "to contain" a substring.
-  await expect(page.locator('body')).toHaveText("レッスン予約");
+  await expect(page.locator("body")).toHaveText("レッスン予約");
 });
--- test/index.test.tsx
+++ test/index.test.tsx
@@ -1,14 +1,13 @@
-import { render, screen } from '@testing-library/react'
+import { render, screen } from "@testing-library/react";
 import React from "react";
 import Page from "../app/page";
 
+describe("Home", () => {
+  it("レッスン予約画面が表示されていること", () => {
+    render(<Page />);
 
-describe('Home', () => {
-  it('レッスン予約画面が表示されていること', () => {
-    render(<Page />)
+    const heading = screen.getByText("レッスン予約");
 
-    const heading = screen.getByText('レッスン予約')
-
-    expect(heading).toBeInTheDocument()
-  })
-})
+    expect(heading).toBeInTheDocument();
+  });
+});
