964d22e0eaee72fb00069ad817c1c84e4c3f7c00
naruhiko-toda <nr27bypphy@gmail.com>
2023-09-02 10:28:21 +0900

    fix e2e test

--- e2e/home.spec.ts
+++ e2e/home.spec.ts
@@ -4,5 +4,5 @@ test("レッスン予約のストーリー", async ({ page }) => {
   await page.goto("localhost:8080/");
 
   // Expect a title "to contain" a substring.
-  await expect(page.locator("body")).toHaveText("レッスン予約");
+  await expect(page.locator("body")).toContainText("レッスン予約");
 });
