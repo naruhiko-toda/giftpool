5e0f30445caee5793e6d64a517ee836edf54b106
naruhiko-toda <nr27bypphy@gmail.com>
2023-09-03 11:11:18 +0900

    refactor: get currency from integer

--- app/components/lessonCard.tsx
+++ app/components/lessonCard.tsx
@@ -1,20 +1,20 @@
 import {Card, CardContent, CardMedia, Divider, Typography} from "@mui/material";
 import {Box} from "@mui/system";
 
-export const LessonCard = ({title, time, price, path, name}): JSX.Element => (
+export const LessonCard = ({ title, time, price, path, name }): JSX.Element => (
   <Card data-testid={`lessonCard_${name}`}>
-    <CardMedia component="img" image={path} alt={name}/>
-    <Divider/>
+    <CardMedia component="img" image={`./images/${path}`} alt={name} />
+    <Divider />
     <CardContent>
       <Typography gutterBottom variant="h5" component="div">
         {title}
       </Typography>
-      <Box sx={{display: "flex", justifyContent: "space-between"}}>
+      <Box sx={{ display: "flex", justifyContent: "space-between" }}>
         <Typography variant="body2" color="text.secondary">
           {time}分/1回
         </Typography>
         <Typography variant="h6" color="#F06292">
-          ￥{price}~
+          {new Intl.NumberFormat("ja-JP", { style: "currency", currency: "JPY" }).format(price)}~
         </Typography>
       </Box>
     </CardContent>
--- app/page.tsx
+++ app/page.tsx
@@ -5,16 +5,16 @@ const Page = (): JSX.Element => (
     <h1>レッスン予約</h1>
     <LessonCard
       title={"コーチングレッスン"}
-      time={"90"}
-      price={"6,000"}
-      path={"./images/coaching.png"}
+      time={90}
+      price={6000}
+      path={"coaching.png"}
       name={"coaching"}
     />
     <LessonCard
       title={"ES添削レッスン"}
-      time={"60"}
-      price={"5,000"}
-      path={"./images/entry_sheet.png"}
+      time={60}
+      price={5000}
+      path={"entry_sheet.png"}
       name={"entrySheet"}
     />
   </>
