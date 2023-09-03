47d981aca29c8a7c719e61cefb3aab4f6e58c87c
naruhiko-toda <nr27bypphy@gmail.com>
2023-09-03 11:31:45 +0900

    get lesson from loop

--- app/page.tsx
+++ app/page.tsx
@@ -1,22 +1,37 @@
 import { LessonCard } from "./components/lessonCard";
+import { Lesson } from "./types/lesson";
 
-const Page = (): JSX.Element => (
-  <>
-    <h1>レッスン予約</h1>
-    <LessonCard
-      title={"コーチングレッスン"}
-      time={90}
-      price={6000}
-      path={"coaching.png"}
-      name={"coaching"}
-    />
-    <LessonCard
-      title={"ES添削レッスン"}
-      time={60}
-      price={5000}
-      path={"entry_sheet.png"}
-      name={"entrySheet"}
-    />
-  </>
-);
-export default Page
+const Page = (): JSX.Element => {
+  const lessons: Lesson[] = [
+    {
+      title: "コーチングレッスン",
+      time: 90,
+      price: 6000,
+      path: "coaching.png",
+      name: "coaching",
+    },
+    {
+      title: "ES添削レッスン",
+      time: 60,
+      price: 5000,
+      path: "entry_sheet.png",
+      name: "entrySheet",
+    },
+  ];
+  return (
+    <>
+      <h1>レッスン予約</h1>
+      {lessons.map((lesson) => (
+        <LessonCard
+          key={lesson.name}
+          title={lesson.title}
+          time={lesson.time}
+          price={lesson.price}
+          path={lesson.path}
+          name={lesson.name}
+        />
+      ))}
+    </>
+  );
+};
+export default Page;
