a5b9e7b0acc2a13aad89f809a7c2abf5722f268b
naruhiko-toda <nr27bypphy@gmail.com>
2023-11-19 21:08:17 +0900

    add vercel build script to enable to run prisma

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,5 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
   content: [
     './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
     './src/components/**/*.{js,ts,jsx,tsx,mdx}',
