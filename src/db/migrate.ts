4260fbd90874244ac686f105f2ff715e74fdf652
naruhiko-toda <nr27bypphy@gmail.com>
2023-12-06 16:56:37 +0900

    fix path

--- src/db/migrate.ts
+++ src/db/migrate.ts
@@ -1,12 +1,7 @@
+import { db } from "@db/database";
 import { migrate } from "drizzle-orm/mysql2/migrator";
-import { drizzle } from "drizzle-orm/postgres-js";
-import postgres from "postgres";
 
 async function main() {
-  const migrationsClient = postgres(process.env.DATABASE_URL, {
-    max: 1,
-  });
-  const db = drizzle(migrationsClient);
   await migrate(db, { migrationsFolder: "./drizzle" });
 }
 
