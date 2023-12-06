6c4885105687b7128bd6f2cbfe65e3b5b9ce7075
naruhiko-toda <nr27bypphy@gmail.com>
2023-12-06 18:11:38 +0900

    update migrate

--- src/db/migrate.ts
+++ src/db/migrate.ts
@@ -1,7 +1,10 @@
-import { db } from "@db/database";
+import { drizzle } from "drizzle-orm/postgres-js";
 import { migrate } from "drizzle-orm/postgres-js/migrator";
+import postgres from "postgres";
 
 async function main() {
+  const sql = postgres(process.env.DATABASE_URL, { max: 1 });
+  const db = drizzle(sql);
   await migrate(db, { migrationsFolder: "./drizzle" });
 }
 
