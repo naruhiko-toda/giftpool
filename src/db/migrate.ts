ec6e028b0dbf47d79ea5d3501f1c817aa7de26df
naruhiko-toda <nr27bypphy@gmail.com>
2023-12-18 21:43:31 +0900

    update migrate

--- src/db/migrate.ts
+++ src/db/migrate.ts
@@ -2,16 +2,18 @@ import { drizzle } from "drizzle-orm/postgres-js";
 import { migrate } from "drizzle-orm/postgres-js/migrator";
 import postgres from "postgres";
 
-async function main() {
-  const sql = postgres(process.env.DATABASE_URL, { max: 1 });
-  const db = drizzle(sql);
-  await migrate(db, { migrationsFolder: "./drizzle" });
-}
-
-try {
-  await main();
-} catch (e) {
-  console.log(e);
-} finally {
-  process.exit(0);
-}
+const sql = postgres(process.env.DATABASE_URL, { max: 1, onnotice: () => {} });
+const db = drizzle(sql);
+await migrate(db, { migrationsFolder: "./drizzle" })
+    .then(() => {
+      console.info("Migration complete")
+    })
+    .catch((error) => {
+      console.error("Migration failed", error)
+      process.exit(1)
+    })
+    .finally(async () => {
+      await sql.end()
+      console.info("Connection closed")
+      process.exit(0)
+    })
