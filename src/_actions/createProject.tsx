6fe31702cd730988c6b0c97c14a4a23b51b537af
naruhiko-toda <nr27bypphy@gmail.com>
2024-02-06 14:19:34 +0900

    add sentry capture

--- src/_actions/createProject.tsx
+++ src/_actions/createProject.tsx
@@ -2,8 +2,10 @@
 import { ProjectRepository } from "@repositories/projectRepository";
 import { CreateProject, Project } from "@type/project";
 import { redirect } from "next/navigation";
+import * as Sentry from "@sentry/nextjs"
 
 const createProject = async (formData: FormData) => {
+  Sentry.captureMessage("Something went wrong");
   const name = formData.get("name") as string;
   const parsedValue = parseInt(formData.get("price") as string, 10);
   const price = !Number.isNaN(parsedValue) ? parsedValue : (0 as number);
