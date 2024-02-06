7b25441602241190c5b7b288966c199db41a4c80
naruhiko-toda <nr27bypphy@gmail.com>
2024-02-06 13:07:48 +0900

    add log to debug sentry

--- sentry.client.config.ts
+++ sentry.client.config.ts
@@ -4,6 +4,9 @@
 
 import * as Sentry from "@sentry/nextjs";
 
+console.log("client")
+console.log(process.env.SENTRY_DSN || process.env.NEXT_PUBLIC_SENTRY_DSN)
+
 Sentry.init({
   dsn: process.env.SENTRY_DSN || process.env.NEXT_PUBLIC_SENTRY_DSN,
 
--- sentry.server.config.ts
+++ sentry.server.config.ts
@@ -4,6 +4,8 @@
 
 import * as Sentry from "@sentry/nextjs";
 
+console.log("server")
+console.log(process.env.SENTRY_DSN || process.env.NEXT_PUBLIC_SENTRY_DSN)
 Sentry.init({
   dsn: process.env.SENTRY_DSN || process.env.NEXT_PUBLIC_SENTRY_DSN,
 
