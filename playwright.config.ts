5f0e820afff8f79d9d06a50ea5aab1f911a6ea51
naruhiko-toda <nr27bypphy@gmail.com>
2024-02-07 20:04:21 +0900

    update package

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,37 +1,18 @@
 import { defineConfig, devices } from "@playwright/test";
-
-/**
- * Read environment variables from file.
- * https://github.com/motdotla/dotenv
- */
-// require('dotenv').config();
-
-/**
- * See https://playwright.dev/docs/test-configuration.
- */
 export default defineConfig({
   testDir: "./e2e",
-  /* Run tests in files in parallel */
   fullyParallel: true,
-  /* Fail the build on CI if you accidentally left test.only in the source code. */
   forbidOnly: !!process.env.CI,
-  /* Retry on CI only */
   retries: process.env.CI ? 2 : 0,
-  /* Opt out of parallel tests on CI. */
   workers: process.env.CI ? 1 : undefined,
-  /* Reporter to use. See https://playwright.dev/docs/test-reporters */
   reporter: process.env.CI
     ? [["html"]]
     : [["html", { host: "0.0.0.0", port: "9323", open: "always" }]],
-  /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     baseURL: "http://test:8081",
-
-    /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: "on-first-retry",
   },
 
-  /* Configure projects for major browsers */
   projects: process.env.CI
     ? [
         {
@@ -61,11 +42,4 @@ export default defineConfig({
           use: { ...devices["Desktop Chrome"] },
         },
       ],
-
-  /* Run your local dev server before starting the tests */
-  // webServer: {
-  //   command: "bun run dev:coverage",
-  //   url: "http://127.0.0.1:8081",
-  //   reuseExistingServer: !process.env.CI,
-  // },
 });
