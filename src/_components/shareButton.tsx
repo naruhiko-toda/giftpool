4580dfc4f15adb5044ab0532aa92fc1097812e29
naruhiko-toda <nr27bypphy@gmail.com>
2024-02-10 23:53:37 +0900

    add share for mobile

--- src/_components/shareButton.tsx
+++ src/_components/shareButton.tsx
@@ -1,16 +1,45 @@
 "use client";
 
+import { ShareModal } from "@components/shareModal";
+import ShareIcon from "@mui/icons-material/Share";
 import { Button } from "@mui/material";
+import React from "react";
 
 export const ShareButton = () => {
+  let title: string;
+  let shareUrl: string;
+  const [open, setOpen] = React.useState(false);
+  const handleOpen = () => setOpen(true);
+
+  if (process.browser) {
+    title = document.title;
+    shareUrl = document.location.href;
+  }
+
   return (
-    <Button
-      variant="outlined"
-      onClick={async () => {
-        await navigator.share({ title: document.title, url: document.location.href });
-      }}
-    >
-      友達にシェア
-    </Button>
+    <>
+      <Button
+        variant="outlined"
+        onClick={async () => {
+          if (navigator.share) {
+            await navigator.share({ title: title, url: shareUrl });
+          } else {
+            handleOpen();
+          }
+        }}
+      >
+        シェア
+        <ShareIcon />
+      </Button>
+
+      <ShareModal
+        open={open}
+        onClose={() => {
+          setOpen(false);
+        }}
+        shareUrl={shareUrl}
+        title={title}
+      />
+    </>
   );
 };
---
+++ test/mock/mockNavigator.tsx
@@ -0,0 +1,14 @@
+export class MockNavigator {
+  shareCallCount: number;
+  shareCallParams: {title: string, url: string};
+
+  constructor() {
+    this.shareCallCount = 0;
+    this.shareCallParams = {title: "", url: ""};
+  }
+
+  share(shareData: { title: string; url: string }) {
+    this.shareCallCount++;
+    this.shareCallParams = shareData;
+  }
+}
